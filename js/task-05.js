71587606b75e18e16a511cd93cfd47112c389551
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-14 23:46:00 +0300

    do 5 task

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', handleEnterInputName);
+
+function handleEnterInputName (event) {
+    spanEl.textContent = event.currentTarget.value;
+
+    if (event.currentTarget.value === "") {
+        spanEl.textContent = "Anonymous"
+    }
+}
