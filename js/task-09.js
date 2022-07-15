63df115efd9f3b472ec90b325ff45be8c267bbbb
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-15 13:23:56 +0300

    do 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', handleChangeBcgColor);
+
+function handleChangeBcgColor(){
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  const newBcgColor = bodyEl.style.backgroundColor;
+  spanEl.innerHTML = newBcgColor;
+}
