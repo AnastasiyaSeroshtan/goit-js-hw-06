d30c289aafd1a2b78bb9bb54679a6c68af981d17
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-16 18:20:14 +0300

    do 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,32 +3,15 @@ function getRandomHexColor() {
 }
 
 const inputEl = document.querySelector('#controls input');
-// console.log(inputEl);
-console.log(inputEl.value);
-
 const btnCreateEl = document.querySelector('button[data-create]');
-// console.log(btnCreateEl);
-
 const btnDestroyEl = document.querySelector('button[data-destroy]');
-// console.log(btnDestroyEl);
-
 const boxesEl = document.querySelector('#boxes');
-// console.log(boxesEl);
-
-// inputEl.addEventListener('input', handleEnterElementInput);
-
 
 btnCreateEl.addEventListener('click', createBoxes);
 btnDestroyEl.addEventListener('click', handleCleanDiv);
 
-function handleCleanDiv () {
-  boxesEl.innerHTML = "";
-}
-
-
-
-function createBoxes(amount) {
-  for (let i = 0; i < amount; i += 1) {
+function createBoxes() {
+  for (let i = 0; i < inputEl.value; i += 1) {
     const box = document.createElement('div');
     console.log(box);
     box.style.width = `${30 + i*10}px`;
@@ -36,11 +19,12 @@ function createBoxes(amount) {
     box.style.backgroundColor = getRandomHexColor();
     boxesEl.appendChild(box);
   }
-  
 }
 
-console.log(createBoxes(5))
-
+function handleCleanDiv () {
+  boxesEl.innerHTML = "";
+  inputEl.value = "";
+}
 
 
 
